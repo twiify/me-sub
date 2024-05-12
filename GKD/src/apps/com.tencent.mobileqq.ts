db70429674a5a7282ef96d3ff3e52707152b5d34
Ticks <ticks.cc@gmail.com>
2024-05-12 21:17:44 +0800

    [add]: GKD-新增QQ规则

--- GKD/src/apps/com.tencent.mobileqq.ts
+++ GKD/src/apps/com.tencent.mobileqq.ts
@@ -2,6 +2,60 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.tencent.mobileqq',
-  name: 'QQ',
-  groups: [],
+  name: 'QQ (NT版本)',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示-隐藏更新提示',
+      desc: '叉掉顶部更新提示',
+      activityIds: ['com.tencent.mobileqq.activity.SplashActivity'],
+      rules: [
+        {
+          key: 1,
+          name: '点击取消按钮',
+          matches: [
+            'LinearLayout > TextView[text="发现QQ版本更新"] - TextView[text="点击下载"] - ImageView',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '其他广告-好友动态-好友热播',
+      activityIds: ['com.qzone.reborn.feedx.activity.QZoneFriendFeedXActivity'],
+      rules: [
+        {
+          key: 1,
+          name: '点击更多',
+          matches: [
+            'LinearLayout > TextView[vid="k2r"][text="好友热播"] - Button[vid="jvj"]',
+          ],
+        },
+        {
+          key: 2,
+          name: '点击更多后-不感兴趣',
+          preKeys: [1],
+          matches: [
+            'LinearLayout[vid="h0f"]  TextView[vid="hhb"][text="不感兴趣"]',
+          ],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '功能优化-图片自动勾选原图',
+      desc: '聊天发送图片自动勾选原图',
+      enable: false,
+      activityIds: ['com.tencent.mobileqq.activity.SplashActivity'],
+      rules: [
+        {
+          key: 1,
+          quickFind: true,
+          matches: [
+            'LinearLayout[vid="jst"] > TextView[vid="p2"][text="相册"] - TextView[vid="ekt"][text="编辑"] - CheckBox[vid="h1y"][checked=false][desc="原图"]',
+          ],
+        },
+      ],
+    },
+  ],
 });
--- GKD/src/subscription.ts
+++ GKD/src/subscription.ts
@@ -6,7 +6,7 @@ import globalGroups from './globalGroups';
 export default defineGkdSubscription({
   id: 7777777,
   name: 'Ticks的GKD订阅',
-  version: 1,
+  version: 2,
   author: 'Ticks',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/ticks-tan/me-sub/GKD',
